aa1d91a39663cf9409ae2e4a9d48473a6c9bd476
apatranx <ap47ra@gmail.com>
2023-01-17 20:16:04 +0700

    update sebelum routing

--- UAS202102260/src/app/mahasiswa/mahasiswa.component.ts
+++ UAS202102260/src/app/mahasiswa/mahasiswa.component.ts
@@ -1,10 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, Renderer2 } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+declare const $ : any;
 
 @Component({
   selector: 'app-mahasiswa',
   templateUrl: './mahasiswa.component.html',
   styleUrls: ['./mahasiswa.component.css']
 })
+
+
 export class MahasiswaComponent implements OnInit {
 
   constructor() { }
